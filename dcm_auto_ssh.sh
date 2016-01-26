fbd2669bb05846766dec44ad94617a3d53a96a3f
hal13 <mitsu_yama007@ae.auone-net.jp>
2016-01-26 21:49:36 +0900

    separate file

---
+++ dcm_auto_ssh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ 
+auto_ssh() {
+HOST=$1
+USER=$2
+PASS=$3
+ 
+expect -c "
+set timeout 10
+spawn ssh ${USER}@${HOST}
+expect \"Are you sure you want to continue connecting (yes/no)?\" {
+    send \"yes\n\"
+    expect \"${USER}@${HOST}'s password:\"
+    send \"${PASS}\n\"
+} \"${USER}@${HOST}'s password:\" {
+    send \"${PASS}\n\"
+}
+interact
+"
+}
